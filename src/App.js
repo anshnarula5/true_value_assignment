0443a6b267b49687bc23f0b569694920d86c0996
Ansh Narula <2019210@iiitdmj.ac.in>
2022-01-05 12:56:00 +0530

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from "react";
+import {Button, Container, Row} from "react-bootstrap";
 
 function App() {
+  const [data, setData] = useState([])
+  useEffect(() => {
+    fetch("https://datapeace-storage.s3-us-west-2.amazonaws.com/dummy_data/users.json")
+    .then(res => res.json())
+    .then(d => setData(d))
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="container">
+      HI
+      <Button className="btn btn-primary">HI</Button>
+    </Container>
   );
 }
 
